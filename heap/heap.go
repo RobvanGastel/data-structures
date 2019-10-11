e08dcee59f7a8bc8c44abdfa758a81a87ef99699
Rob van Gastel <robvangastel@hotmail.com>
2019-10-11 10:38:12 +0200

    Forgot pointers and dereferences, its been a while

--- heap/heap.go
+++ heap/heap.go
@@ -12,50 +12,50 @@ func Parent(i int) int {
 	return int(math.Floor(float64(i-1)/2.0))
 }
 
-func Left(i int) int {
-	return 2*i
+func Left(i *int) *int {
+	l := 2*(*i)
+	return &l
 }
 
-func Right(i int) int {
-	return 2*i+1
+func Right(i *int) *int {
+	r := 2*(*i+1)
+	return &r
+
 }
 
-func HeapMaximum(A []int) int {
-	return A[1]
+func HeapMaximum(A []int) *int {
+	return &A[1]
 }
 
-func MaxHeapify(A []int, i int) {
+func MaxHeapify(A []int, i *int) {
+	// i = i
 	l := Left(i)
 	r := Right(i)
 	largest := 0
 
-	fmt.Println(largest)
-	fmt.Println(r)
-	fmt.Println(l)
-
-	if l <= len(A) && A[l] > A[i] {
-		largest = l
+	if *l <= len(A) && A[*l] > A[*i] {
+		largest = *l
 	} else {
-		largest = i
+		largest = *i
 	}
 
-	if r <= len(A) && A[r] > A[largest] {
-		largest = r
+	if *r <= len(A)-1 && A[*r] > A[largest] {
+		largest = *r
 	}
 
-	if largest != i {
+	if largest != *i {
 		temp := A[largest]
-		A[largest] = A[i]
-		A[i] = temp
-		MaxHeapify(A, largest)
+		A[largest] = A[*i]
+		A[*i] = temp
+		MaxHeapify(A, &largest)
 	}
 }
 
 func BuildMaxHeap(A []int) {
-	A_floor := int(math.Floor(float64(len(A)))/2.0)
+	A_floor := int(math.Floor(float64(len(A)))/2.0)-1
 	for i := A_floor; i > 1; i-- {
 		fmt.Println(A_floor, i)
-		MaxHeapify(A, i)
+		MaxHeapify(A, &i)
 	}
 }
 
@@ -65,27 +65,29 @@ func HeapExtractMax(A []int) (int, error) {
 	}
 
 	max := A[1]
-	MaxHeapify(A, 1)
+	i := 1
+	MaxHeapify(A, &i)
 	return max, nil
 }
 
-func HeapIncreaseKey(A []int, i int, key int) error {
-	if key < A[i] {
-		return errors.New("new key is smaller than current key")
-	}
+// func HeapIncreaseKey(A []int, i *int, key *int) error {
+// 	i = i
+// 	if key < A[i] {
+// 		return errors.New("new key is smaller than current key")
+// 	}
 
-	A[i] = key
-	for i > 1 && A[Parent(i)] < A[i] {
-		temp := A[Parent(i)]
-		A[Parent(i)] = A[i]
-		A[i] = temp
+// 	A[&i] = key
+// 	for i > 1 && A[Parent(i)] < A[i] {
+// 		temp := A[Parent(i)]
+// 		A[Parent(i)] = A[i]
+// 		A[i] = temp
 
-		i = Parent(i)
-	}
-	return nil
-}
+// 		i = Parent(i)
+// 	}
+// 	return nil
+// }
 
-func MaxHeapInsert(A []int, key int) {
-	A = append(A, -2147483648)
-	HeapIncreaseKey(A, len(A), key)
-}
+// func MaxHeapInsert(A []int, key *int) {
+// 	A = append(A, -2147483648)
+// 	HeapIncreaseKey(A, len(A), key)
+// }

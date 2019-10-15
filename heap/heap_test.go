14ef460342fec53e8c4eb52f8524e285c9a0fea2
Rob van Gastel <robvangastel@hotmail.com>
2019-10-15 11:11:04 +0200

    Improved tests

--- heap/heap_test.go
+++ heap/heap_test.go
@@ -1,9 +1,9 @@
 package heap
 
 import (
-	"testing"
 	"math/rand"
 	"reflect"
+	"testing"
 )
 
 // TODO: Create generic method, Checks if values are equal
@@ -15,60 +15,47 @@ func assertEqual(t *testing.T, a interface{}, b interface{}) {
 	t.Errorf("Received %v (type %v), expected %v (type %v)", a, reflect.TypeOf(a), b, reflect.TypeOf(b))
 }
 
-// TODO: Implement Tests on Insert, Delete, Transplant
-
+// TODO: Improve benchmark
 func BenchmarkInsert(*testing.B) {
 	A := make([]int, 0)
 	// Fill max heap
-	for i := 0; i< 1000; i++ {
+	for i := 0; i < 1000; i++ {
 		A = append(A, rand.Intn(30))
 	}
 
 	BuildMaxHeap(&A) // Build inplace
 }
 
+// Valid date if it builds a valid Max Heap
 func TestCheckMaxHeap(t *testing.T) {
 	A := make([]int, 0)
-	// Fill max heap
 	for i := 0; i < 1000; i++ {
 		randm := rand.Intn(1000)
 		A = append(A, randm)
 	}
 	BuildMaxHeap(&A) // Build inplace
 
-	valid_heap := true
-	prev_max, _ := HeapExtractMax(&A)
+	validHeap := true
+	prevMax, _ := HeapExtractMax(&A)
 	n := len(A)
 	for j := 0; j < n; j++ {
 		max, _ := HeapExtractMax(&A)
 
-		if prev_max < max {
-			valid_heap = false
+		if prevMax < max {
+			validHeap = false
 			break
 		}
 	}
 
-    assertEqual(t, true, valid_heap)
+	assertEqual(t, true, validHeap)
 }
 
 func TestCheckIncreaseKeyEmptyHeap(t *testing.T) {
-	// A := make([]int, 0)
-
-	// // Fill max heap
-	// for i := 0; i < 1000; i++ {
-	// 	randm := rand.Intn(1000)
-	// 	A = append(A, randm)
-	// }
-
-	// BuildMaxHeap(A) // Build inplace
-
-    // AssertEqual(t, true, valid_heap)
+	// TODO
 }
 
 func TestCheckIncreaseKey(t *testing.T) {
 	A := make([]int, 0)
-
-	// Fill max heap
 	for i := 0; i < 1000; i++ {
 		randm := rand.Intn(1000)
 		A = append(A, randm)
@@ -77,5 +64,5 @@ func TestCheckIncreaseKey(t *testing.T) {
 
 	val := 10000
 	errorOrNil := MaxHeapInsert(&A, &val)
-    assertEqual(t, nil, errorOrNil)
+	assertEqual(t, nil, errorOrNil)
 }
